0888a5c83fd73a785b31af8e0afb77c0e3b26b18
Jacob Marthaller <https://github.com/jmarthaller>
2020-12-14 16:46:50 -0500

    final functionality

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,55 @@
 class EmployeesController < ApplicationController
+    
+    
+    def index
+        @employees = Employee.all
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+    end
+
+    def new
+        @employee = Employee.new
+        
+        #render :new
+    end 
+    
+    def create 
+        @employee = Employee.create(employee_params)
+        #byebug
+        #@employee.save
+        redirect_to employees_path(@employee)
+    end 
+    
+    def edit
+        @employee = Employee.find(params[:id])
+        
+        # render :edit
+    end 
+    
+    def update
+        @employee = Employee.find(params[:id])
+        @employee.update(employee_params)
+        redirect_to employees_path(@employee)
+    end 
+
+     
+
+    private 
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :office, :img_url, :dog_id, :title, :alias)
+    end 
+
+    # create_table "employees", force: :cascade do |t|
+    #     t.string "first_name"
+    #     t.string "last_name"
+    #     t.string "alias"
+    #     t.string "title"
+    #     t.string "office"
+    #     t.string "img_url"
+    #     t.integer "dog_id", null: false
+     
+
 end
